3919cc1fa961eb2a8f4dde3e39b2f9c810d77fc4
Leonardo Lenz Barbosa <38910205+tiddez@users.noreply.github.com>
2023-11-09 15:20:08 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
-screen -dmS cod4 ./cod4x18_dedrun +set dedicated 2 +set net_port 28960 +set sv_punkbuster 1 +set fs_game mods/pml220pub +exec server.cfg +set rcon_password 12345678 +map_rotate
+screen -dmS cod4 ./cod4x18_dedrun +set dedicated 2 +set net_port 28960 +set sv_punkbuster 1 +set fs_game mods/pml220pub +exec server.cfg +set rcon_password qwe12345678 +map_rotate
+
+#screen -dmS cod4 ./cod4x18_dedrun +set dedicated 2 +set net_port 28960 +set sv_punkbuster 1 +set fs_game mods/pml220pub +exec server.cfg +set rcon_password 12345678 +map_rotate
 #screen -dmS cod4 ./cod4x18_dedrun +set dedicated 2 +set sv_authorizemode 0 +set rcon_password seekers123 +set sv_punkbuster 1 +exec server.cfg +set sv_maxclient 32 +set net_port 28960 +set fs_basepath /home/servers/cod4 +set fs_homepath /home/servers/cod4 +set fs_game mods/pml220 +map_rotate
